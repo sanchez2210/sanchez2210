109f621370e7f9f49090b65ff2c99bcae90f71c7
Luis Felipe Sanchez <luisfelipe22@outlook.com>
2017-03-27 06:34:40 -0500

    made fullscreen slides and trimmed code

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 
-var files;
 var songsAmount=0;
 var imagesAmount=0;
 var trackNumber = 0;
 var source = document.getElementById("playersource");
 var player = document.getElementById("player");
-var songs = document.getElementById("songs");
 var shuffle = false;
-var filepicker = document.getElementById("filepicker");
-var nextButton = document.getElementById("next");
-var prevButton = document.getElementById("prev");
-var shuffleButton = document.getElementById("shuffle");
-var imagesContainter = document.getElementById("imagesContainer");
+var imagesContainer = document.getElementById("imagesContainer");
 var console = document.getElementById("console");
 
 var req = new XMLHttpRequest();
@@ -33,7 +27,7 @@ var jssor_1_slider_init = function() {
 
     var jssor_1_options = {
         $AutoPlay: true,
-        $FillMode: "3",
+        $FillMode: 1,
         $SlideshowOptions: {
         $Class: $JssorSlideshowRunner$,
         $Transitions: jssor_1_SlideshowTransitions,
@@ -49,43 +43,9 @@ var jssor_1_slider_init = function() {
 
     var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
 
-    /*responsive code begin*/
-    /*remove responsive code if you don't want the slider scales while window resizing*/
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 600);
-            jssor_1_slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $Jssor$.$AddEvent(window, "load", ScaleSlider);
-    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*responsive code end*/
-};
-
 
 function init() { //This code starts everything as soon as files.json is read
     trackNumber = 0;
-    // songs.innerHTML = "";
-    // for (var i = 0; i< songsAmount ; i++) {
-
-    //     var li = document.createElement("li");
-    //     li.setAttribute("class", "song");
-    //     li.innerHTML= i + ".mp3";
-    //     li.id = i;
-
-    //     li.addEventListener("click", (function(i){
-    //         trackNumber=i;
-    //         playTrack(i);
-    //     }).bind(null,i),false);
-
-    //     songs.appendChild(li);
-    // }
 
     for(var j = 0; j < imagesAmount ; j++){
 
@@ -94,17 +54,14 @@ function init() { //This code starts everything as soon as files.json is read
         img.setAttribute("data-u","image");
         img.src = "img/" + j + ".jpg";
         div.appendChild(img);
-        imagesContainter.appendChild(div);
+        imagesContainer.appendChild(div);
 
     }
     jssor_1_slider_init();
     loadTrack(trackNumber);
 };
 
-// prevButton.addEventListener("click", prevSong,false);
-// nextButton.addEventListener("click", nextSong,false);
 player.addEventListener("ended", nextSong, false);
-// shuffleButton.addEventListener("click",toggleShuffle,false);
 
 
 function prevSong(){
